cc64b43d02fda12ed58ac74d71ef4fd3c010cccf
oh-yes-0-fps <foryt.bowan@gmail.com>
2023-06-27 22:03:45 -0400

    added some math enums

--- wpilib/src/math/units.rs
+++ wpilib/src/math/units.rs
@@ -10,7 +10,6 @@ pub type Radians = f64;
 pub type RadiansPerSecond = f64;
 pub type RadiansPerSecondSquared = f64;
 
-
 //imperial
 pub type Feet = f64;
 pub type FeetPerSecond = f64;
@@ -52,3 +51,66 @@ pub fn seconds_to_hours(seconds: Seconds) -> Hours {
     seconds / 3600.0
 }
 
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum Distance {
+    Meters(Meters),
+    Feet(Feet),
+    Inches(Inches),
+}
+impl Distance {
+    pub fn to_meter(&self) -> Meters {
+        match self {
+            Distance::Meters(m) => *m,
+            Distance::Feet(f) => feet_to_meters(*f),
+            Distance::Inches(i) => feet_to_meters(*i / 12.0),
+        }
+    }
+
+    pub fn to_feet(&self) -> Feet {
+        match self {
+            Distance::Meters(m) => meters_to_feet(*m),
+            Distance::Feet(f) => *f,
+            Distance::Inches(i) => *i / 12.0,
+        }
+    }
+
+    pub fn to_inch(&self) -> Inches {
+        match self {
+            Distance::Meters(m) => meters_to_feet(*m) * 12.0,
+            Distance::Feet(f) => *f * 12.0,
+            Distance::Inches(i) => *i,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum Angle {
+    Radians(Radians),
+    Degrees(Degrees),
+    Rotations(f64),
+}
+impl Angle {
+    pub fn to_radians(&self) -> Radians {
+        match self {
+            Angle::Radians(r) => *r,
+            Angle::Degrees(d) => degrees_to_radians(*d),
+            Angle::Rotations(r) => r * 2.0 * PI,
+        }
+    }
+
+    pub fn to_degrees(&self) -> Degrees {
+        match self {
+            Angle::Radians(r) => radians_to_degrees(*r),
+            Angle::Degrees(d) => *d,
+            Angle::Rotations(r) => r * 360.0,
+        }
+    }
+
+    pub fn to_rotations(&self) -> f64 {
+        match self {
+            Angle::Radians(r) => r / (2.0 * PI),
+            Angle::Degrees(d) => d / 360.0,
+            Angle::Rotations(r) => *r,
+        }
+    }
+}
